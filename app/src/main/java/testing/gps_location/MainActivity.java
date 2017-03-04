a3803498c08a52efe60c75e88c434d1729d2dc23
andreupav <andreu.pavia@est.fib.upc.edu>
2017-03-04 16:35:16 +0100

    Nice

--- app/src/main/java/testing/gps_location/MainActivity.java
+++ app/src/main/java/testing/gps_location/MainActivity.java
@@ -1,47 +1,54 @@
 package testing.gps_location;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
+        import java.net.InetSocketAddress;
+        import java.net.Socket;
+        import java.net.SocketAddress;
+        import java.util.Calendar;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Calendar;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.Manifest;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.location.Location;
+        import android.location.LocationListener;
+        import android.location.LocationManager;
+        import android.os.AsyncTask;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.provider.Settings;
+        import android.support.annotation.NonNull;
+        import android.support.v4.app.ActivityCompat;
+        import android.util.Log;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    Button btnStart;
+    Button btnSend;
+    TextView textStatus;
+    NetworkTask networktask;
     private Button b;
     private TextView t;
     private LocationManager locationManager;
     private LocationListener listener;
     int count =0;
-
+    String data;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        t = (TextView) findViewById(R.id.textView);
-        b = (Button) findViewById(R.id.button);
-
+        setContentView(R.layout.main);
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnSend = (Button)findViewById(R.id.btnSend);
+        textStatus = (TextView)findViewById(R.id.textStatus);
+        btnStart.setOnClickListener(btnStartListener);
+        btnSend.setOnClickListener(btnSendListener);
+        networktask = new NetworkTask(); //Create initial instance so SendDataToNetwork doesn't throw an error.
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
@@ -67,17 +74,14 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String s) {
-
                 Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(i);
             }
         };
-
         configure_button();
     }
     public void ra(Location location){
-        t = (TextView) findViewById(R.id.textView);
-        b = (Button) findViewById(R.id.button);
+
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
@@ -88,23 +92,38 @@ public class MainActivity extends AppCompatActivity {
         int day = c.get(Calendar.DAY_OF_MONTH);
         int month = c.get(Calendar.MONTH);
         int year = c.get(Calendar.YEAR);
-        t.append("\n " + location.getLongitude() + " " + location.getLatitude());
-        t.append("\n " + day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds);
+        /*t.append("\n " + location.getLatitude() + ";" + location.getLongitude());*/
+        data =location.getLatitude() + ";" + location.getLongitude() + ";"+ day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds;
+        /*t.append("\n " + day + "/" + month + "/" + year + " " + hour + ":" + minutes + ":" + seconds);*/
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
             case 10:
-                    configure_button();
+                configure_button();
                 break;
             default:
                 break;
         }
     }
 
+    private OnClickListener btnStartListener = new OnClickListener() {
+        public void onClick(View v){
+            btnStart.setVisibility(View.INVISIBLE);
+            networktask = new NetworkTask(); //New instance of NetworkTask
+            networktask.execute();
+        }
+    };
+    OnClickListener btnSendListener = new OnClickListener() {
+        public void onClick(View v){
+            textStatus.setText("Sending your location for emergency");
+            count = 0;
+            //noinspection MissingPermission
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+            networktask.SendDataToNetwork(data);
+        }
+    };
     void configure_button(){
-        // first check for permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
@@ -112,36 +131,104 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
-        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count = 0;
-                //noinspection MissingPermission
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 0, listener);
+    }
 
+    public class NetworkTask extends AsyncTask<Void, byte[], Boolean> {
+        Socket nsocket; //Network Socket
+        InputStream nis; //Network Input Stream
+        OutputStream nos; //Network Output Stream
 
-                Socket socket = null;
+        @Override
+        protected void onPreExecute() {
+            Log.i("AsyncTask", "onPreExecute");
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) { //This runs on a different thread
+            boolean result = false;
+            try {
+                Log.i("AsyncTask", "doInBackground: Creating socket");
+                SocketAddress sockaddr = new InetSocketAddress("10.4.180.92", 5354);
+                nsocket = new Socket();
+                nsocket.connect(sockaddr, 5000); //10 second connection timeout
+                if (nsocket.isConnected()) {
+                    nis = nsocket.getInputStream();
+                    nos = nsocket.getOutputStream();
+                    Log.i("AsyncTask", "doInBackground: Socket created, streams assigned");
+                    Log.i("AsyncTask", "doInBackground: Waiting for inital data...");
+                    byte[] buffer = new byte[4096];
+                    int read = nis.read(buffer, 0, 4096); //This is blocking
+                    while(read != -1){
+                        byte[] tempdata = new byte[read];
+                        System.arraycopy(buffer, 0, tempdata, 0, read);
+                        publishProgress(tempdata);
+                        Log.i("AsyncTask", "doInBackground: Got some data");
+                        read = nis.read(buffer, 0, 4096); //This is blocking
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("AsyncTask", "doInBackground: IOException");
+                result = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("AsyncTask", "doInBackground: Exception");
+                result = true;
+            } finally {
                 try {
-                    socket = new Socket("127.0.0.1", 50505);
-                    Toast.makeText(MainActivity.this, "MESSAGING", Toast.LENGTH_LONG).show();
-                    OutputStream out = socket.getOutputStream();
-                    PrintWriter output = new PrintWriter(out);
-
-                    //mStatusText.setText("Sending Data to PC");
-                    output.println("Hello from Android");
-                    //mStatusText.setText("Data sent to PC");
-                    out.flush();
-                    out.close();
-
-                    socket.close();
-                   // mStatusText.setText("Socket closed");
+                    nis.close();
+                    nos.close();
+                    nsocket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
+                Log.i("AsyncTask", "doInBackground: Finished");
             }
-        });
+            return result;
+        }
+
+        public void SendDataToNetwork(String cmd) { //You run this from the main thread.
+            try {
+                if (nsocket.isConnected()) {
+                    Log.i("AsyncTask", "SendDataToNetwork: Writing received message to socket");
+                    nos.write(cmd.getBytes());
+                } else {
+                    Log.i("AsyncTask", "SendDataToNetwork: Cannot send message. Socket is closed");
+                }
+            } catch (Exception e) {
+                Log.i("AsyncTask", "SendDataToNetwork: Message send failed. Caught an exception");
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(byte[]... values) {
+            if (values.length > 0) {
+                Log.i("AsyncTask", "onProgressUpdate: " + values[0].length + " bytes received.");
+                textStatus.setText(new String(values[0]));
+            }
+        }
+        @Override
+        protected void onCancelled() {
+            Log.i("AsyncTask", "Cancelled.");
+            btnStart.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Log.i("AsyncTask", "onPostExecute: Completed with an Error.");
+                textStatus.setText("There was a connection error.");
+            } else {
+                Log.i("AsyncTask", "onPostExecute: Completed.");
+            }
+            btnStart.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        networktask.cancel(true); //In case the task is currently running
     }
 }
-
-
